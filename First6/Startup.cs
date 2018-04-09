139243ccd33e563781ed8a0a92631c66124a74c8
Fawzi Benamor <f.benamor@root.sa>
2018-04-09 17:51:00 +0100

    Add project files.

---
+++ First6/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(First6.Startup))]
+namespace First6
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
